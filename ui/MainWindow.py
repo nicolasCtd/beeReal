e0fa5180e661540fd180b8b6d0dda1b97cc41db4
OverSpeedUp <jeremy.chesnel@gmail.com>
2025-11-06 22:52:42 +0100

    Start work on analysis file loading

--- ui/MainWindow.py
+++ ui/MainWindow.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 from PyQt5.QtGui import QIcon
 from ui import MainWindow_ui as ui
 from ui import AnalysisSettingForm as ASF
+from PyQt5.QtCore import pyqtSlot
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -14,4 +16,24 @@ class MainWindow(QMainWindow):
         #self.setWindowIcon(QIcon(":/images/deco1.jpg"))        
         self.analysisForm = ASF.AnalysisSettingForm()
         #self.analysisForm .setEnabled(False)
-        self.setCentralWidget(self.analysisForm)
+        #self.setCentralWidget(self.analysisForm)
+        self.ui.analysisVerticalLayout.addWidget(self.analysisForm)
+
+        # Some shortcuts
+        self.ui.actionLoad_analysis.setShortcut("Ctrl+O")
+
+        # Connection
+        self.ui.actionLoad_analysis.triggered.connect(self.loadAnalysis)
+
+    @pyqtSlot()
+    def loadAnalysis(self):
+        print("Loading analysis")
+        # QFileDialog renvoie un tuple (chemin, filtre)
+        chemin, _ = QFileDialog.getOpenFileName(
+            self,
+            "Open analysis file",
+            "",
+            "xml file (*.xml);; bee file (*.bee)"
+        )
+
+        return
