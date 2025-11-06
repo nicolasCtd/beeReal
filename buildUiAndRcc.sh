61ecc72583d865bd98f009ffece9a699fdf0cc95
Jeremy <jeremy.chesnel@meteo.fr>
2025-11-06 10:49:20 +0100

    Add sh script for building py ui and resources

---
+++ buildUiAndRcc.sh
@@ -0,0 +1,3 @@
+pyuic5 ./ui/MainWindow.ui -o ./ui/MainWindow_ui.py
+pyuic5 ./ui/AnalysisSettingForm.ui -o ./ui/AnalysisSettingForm_ui.py
+pyrcc5 ./resources/resources.qrc -o ./resources/resources_rc.py
