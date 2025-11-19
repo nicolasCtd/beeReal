6d9f5cc3c14b4a5830e3f0d902b3970a442927ae
OverSpeedUp <jeremy.chesnel@gmail.com>
2025-11-19 22:25:36 +0100

    Add contextual menu for the row of the table view

--- ui/beeTableView.py
+++ ui/beeTableView.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QTableView
+from PyQt5.QtWidgets import QTableView, QMenu
+from PyQt5.QtCore import QPoint, Qt
 from models.beeItemModel import BeeItemModel
+
 
 
 def is_image_file(path):
@@ -10,8 +12,38 @@ class BeeTableView(QTableView):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setAcceptDrops(True)
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.openMenu)
 
         return
+
+    def openMenu(self, pos: QPoint):
+        index = self.indexAt(pos)
+        if not index.isValid():
+            return
+
+        row = index.row()
+
+        # menu
+        menu = QMenu(self)
+
+        act_view = menu.addAction("Voir l'image")
+        act_mark = menu.addAction("Marquer pour retraiter")
+        act_delete = menu.addAction("Supprimer")
+
+        action = menu.exec_(self.mapToGlobal(pos))
+        if action is None:
+            return
+
+        #if action == act_view:
+            #self.parent().viewImage(row)
+
+        #elif action == act_mark:
+            #self.parent().markRowForRetry(row)
+
+        #elif action == act_delete:
+            #self.parent().deleteRow(row)
+
 
     def dragEnterEvent(self, event):
         # On accepte uniquement les fichiers
