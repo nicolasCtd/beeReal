b85b92a5a5650a924ab3e257069a326b416c4bb2
OverSpeedUp <jeremy.chesnel@gmail.com>
2025-12-22 19:56:51 +0100

    Manually mange panning

--- ui/CustomGraphicsView.py
+++ ui/CustomGraphicsView.py
@@ -22,8 +22,14 @@ class CustomGraphicsView(QGraphicsView):
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
 
         self.zoomLevel = 0
-        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)    
-        
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self._panning = True
+            self._last_mouse_pos = event.pos()
+            event.accept()
+        else:
+            super().mousePressEvent(event)
 
 
     def wheelEvent(self, event):
@@ -60,7 +66,20 @@ class CustomGraphicsView(QGraphicsView):
         event.accept()
 
 
+    def mouseMoveEvent(self, event):
+        if self._panning:
+            old_scene_pos = self.mapToScene(self._last_mouse_pos)
+            new_scene_pos = self.mapToScene(event.pos())
+            delta = new_scene_pos - old_scene_pos
+            self.translate(delta.x(), delta.y())
+            self._last_mouse_pos = event.pos()
+        else:
+            super().mouseMoveEvent(event)
+
     def mouseReleaseEvent(self, event):
+
+        if event.button() == Qt.LeftButton:
+            self._panning = False
 
         if event.modifiers() == addPointModifier:
             posImage = self.mapToScene(event.pos())
