65c1f467ee9629ec1790431b510ba006f628611a
OverSpeedUp <jeremy.chesnel@gmail.com>
2026-03-23 19:05:12 +0100

    Enhance image display management in imageViewer

--- ui/ImageViewer.py
+++ ui/ImageViewer.py
@@ -1,25 +1,51 @@
 from ui import ImageViewer_ui as ui
 from PyQt5.QtWidgets import QWidget, QGraphicsScene
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QRectF
 
 class ImageViewer(QWidget):
-    def __init__(self, image_path : str, parent=None, showAnalysisPannel : bool = False):
+    def __init__(self, image_path: str, parent=None, showAnalysisPannel: bool = True):
         super().__init__(parent)
-        self.setWindowFlag(Qt.Window) # Autonomous windows
+        self.setWindowFlag(Qt.Window)
         self.ui = ui.Ui_ImageViewer()
         self.ui.setupUi(self)
-        self.scene = QGraphicsScene(self.ui.graphicsView)
-        self.ui.graphicsView.setScene(self.scene)
-
-        self.pixmap = QPixmap(image_path)
-        self.image_item = self.scene.addPixmap(self.pixmap)
-
-        # Adapt view to image
-        img_w = self.pixmap.width()
-        img_h = self.pixmap.height()
-        self.ui.graphicsView.setMinimumSize(img_w, img_h)
-
-        self.ui.analysisFrame.setHidden(not showAnalysisPannel)
         
+        initial_size =print(self.size())
 
+        # Setup Scene
+        self.scene = QGraphicsScene(self)
+        self.ui.graphicsView.setScene(self.scene)
+        
+        # Load Image
+        self.pixmap = QPixmap(image_path)
+        if not self.pixmap.isNull():
+
+            target_width = self.width() 
+            ratio = self.pixmap.height() / self.pixmap.width()
+            target_height = int(target_width * ratio)
+
+            if showAnalysisPannel:
+                target_width += self.ui.analysisFrame.width()
+
+            self.resize(target_width, target_height)
+
+
+            self.image_item = self.scene.addPixmap(self.pixmap)
+            self.scene.setSceneRect(QRectF(self.pixmap.rect()))
+            
+            # Initial fit (use a small delay or call after show() for best results)
+            self.ui.graphicsView.fitInView(self.image_item, Qt.KeepAspectRatio)
+        
+        # UI State
+        self.ui.analysisFrame.setHidden(not showAnalysisPannel)
+
+    def showEvent(self, event):
+        if (self.image_item):
+            # This ensures the fitInView works once the geometry is calculated
+            self.ui.graphicsView.fitInView(self.image_item, Qt.KeepAspectRatio)
+            super().showEvent(event)
+
+    def resizeEvent(self, event):
+        if (self.image_item):
+            super().resizeEvent(event)
+            self.ui.graphicsView.fitInView(self.image_item, Qt.KeepAspectRatio)
